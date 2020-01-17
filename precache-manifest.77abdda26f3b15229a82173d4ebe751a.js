726c084c16b7b6082904f7cdb96eb396f4a43c4c
Aman Jain <amanjain.3331@gmail.com>
2020-01-17 16:34:44 +0530

    first commit

---
+++ precache-manifest.77abdda26f3b15229a82173d4ebe751a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6c2f640cfd982fd8e9f2",
+    "url": "/static/js/main.6c2f640c.chunk.js"
+  },
+  {
+    "revision": "ed576f01e9f4eba9fb85",
+    "url": "/static/js/2.ed576f01.chunk.js"
+  },
+  {
+    "revision": "6c2f640cfd982fd8e9f2",
+    "url": "/static/css/main.6c86ee29.chunk.css"
+  },
+  {
+    "revision": "ed576f01e9f4eba9fb85",
+    "url": "/static/css/2.6cf493dd.chunk.css"
+  },
+  {
+    "revision": "3ae2e74d4d39c1c875d1090077b8439c",
+    "url": "/index.html"
+  }
+];
